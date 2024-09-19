550198c6f9b688d65089b8a9f50ca7f0573e35e6
Nelson-Levi <nellevi@byui.edu>
2024-09-19 10:25:07 -0600

    Completed Prep 1 and Prep 2 assignments.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name? "); // Use Console.Write, NOT Console.Writeline for user input
+        string firstName = Console.ReadLine(); // Define the variable type, then set it equal to the Console.ReadLine function
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. "); // Display the results to the user, using $ for string interpolation
+        
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage? ");
+        string gradeInput = Console.ReadLine();
+        int grade = int.Parse(gradeInput);
+        string letterGrade = "";
+
+        int lastDigit = grade % 10;
+        string sign = "";
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+
+        else if (lastDigit <= 3)
+        {
+            sign = "-";
+        }
+
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+
+            if (sign == "+") sign = "";
+        }
+
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else
+        {
+            letterGrade = "F";
+            sign = "";
+        }
+
+        Console.WriteLine($"You have a {letterGrade}{sign} in this class. ");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You are passing the class! ");
+        }
+
+        else
+        {
+            Console.WriteLine("You are not passing. Try again next semester! ");
+        }
     }
 }

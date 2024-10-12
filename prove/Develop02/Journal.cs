56b633c9b9813b6742192ceed6554a9405ac8902
Nelson-Levi <nellevi@byui.edu>
2024-10-12 17:12:30 -0600

    Completed Develop02

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,30 +1,42 @@
-using System.IO; 
+using System.IO;
 using System.Collections.Generic;
+using System.Collections;
 
 public class Journal
 {
 
-    public void saveFile(List<string> _completedEntries, string _filename)
+    public void SaveFile(List<string> _completedEntries, string _filename)
     {
         using (StreamWriter outputfile = new StreamWriter(_filename))
         {
+            // Write every entry in the list to the desired file.
             foreach (string entry in _completedEntries)
             {
-            outputfile.WriteLine(entry);
+                outputfile.WriteLine(entry);
             }
+            // Clear out all the buffered data to prevent errors.
             outputfile.Flush();
+            // Display file location.
             string currentDirectory = Directory.GetCurrentDirectory();
             Console.WriteLine($"Wrote to: {currentDirectory}\\{_filename}");
         }
     }
 
-    public void loadFile(string _filename, Entry entry)
+    public void LoadFile(string _filename, Entry entry)
     {
-    string[] lines = System.IO.File.ReadAllLines(_filename);
-    foreach (string line in lines)
-    {
-        entry._completedEntries.Add(line);
-        Console.WriteLine(line);
-    }
+        if (File.Exists(_filename))
+        {
+            string[] lines = System.IO.File.ReadAllLines(_filename);
+            foreach (string line in lines)
+            {
+                // Add the entries in the loaded file to the _completedEntries list, then display them.
+                entry._completedEntries.Add(line);
+                Console.WriteLine(line);
+            }
+        }
+        else
+        {
+            Console.WriteLine($"{_filename} does not exist.");
+        }
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,6 +1,14 @@
+// Additions to exceed requirements:
+// * Added functionality to display file location when a file is saved.
+// * Calculated and saved the time spent writing each journal entry. This displays alongside each entry.
+
 // Websites I used for help:
 // https://www.tutorialspoint.com/how-to-select-a-random-element-from-a-chash-list
 // https://stackoverflow.com/questions/24443827/reading-an-integer-from-user-input
+// https://learn.microsoft.com/en-us/dotnet/api/system.io.filestream.flush?view=net-8.0
+// https://stackoverflow.com/questions/1026841/how-to-get-only-time-from-date-time-c-sharp
+// https://learn.microsoft.com/en-us/dotnet/api/system.datetime.toshorttimestring?view=net-8.0&redirectedfrom=MSDN#System_DateTime_ToShortTimeString
+// https://www.c-sharpcorner.com/blogs/timespan-in-c-sharp
 
 using System;
 
@@ -15,13 +23,14 @@ class Program
         // You should initialize these outside of the loop.
         Entry entry = new Entry();
         Journal journal = new Journal();
-        
+
         while (userSelection != 5)
         {
             Console.WriteLine("Please select one of the following choices. ");
-            Console.WriteLine(" 1. Write \n 2. Display \n 3. Load \n 4. Save \n 5. Quit");
+            Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save \n5. Quit");
+            Console.Write("What would you like to do? ");
             string input = Console.ReadLine();
-            
+
             // Try parsing the input to an integer
             if (int.TryParse(input, out userSelection))
             {
@@ -29,36 +38,36 @@ class Program
                 {
                     // Write
                     case 1:
-                    entry.journalEntry();
-                    break;
+                        entry.JournalEntry();
+                        break;
 
                     // Display
                     case 2:
-                    entry.journalDisplay();
-                    break;
+                        entry.JournalDisplay();
+                        break;
 
                     // Load
                     case 3:
-                    Console.WriteLine("Enter a file to load. ");
-                    string loadingFile = Console.ReadLine();
-                    journal.loadFile(loadingFile, entry);
-                    break;
+                        Console.WriteLine("Enter a file to load. ");
+                        string loadingFile = Console.ReadLine();
+                        journal.LoadFile(loadingFile, entry);
+                        break;
 
                     // Save
                     case 4:
-                    Console.WriteLine("Enter a filename to save entries:");
-                    string filename = Console.ReadLine();
-                    journal.saveFile(entry._completedEntries, filename);
-                    break;
+                        Console.WriteLine("Enter a filename to save entries:");
+                        string filename = Console.ReadLine();
+                        journal.SaveFile(entry._completedEntries, filename);
+                        break;
 
                     // End program
                     case 5:
-                    Console.WriteLine("Thank you for using the journal program. Have a great day! ");
-                    break;
+                        Console.WriteLine("Thank you for using the journal program. Have a great day!\n");
+                        break;
 
                     default:
-                    Console.WriteLine("You entered an invalid option. Please enter a number from 1-5. ");
-                    break;
+                        Console.WriteLine("You entered an invalid option. Please enter a number from 1-5. ");
+                        break;
                 }
             }
 

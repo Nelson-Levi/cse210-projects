7ca6f558b6ecbcd7181d78bc76d0c5aa07b68538
Nelson-Levi <nellevi@byui.edu>
2024-10-29 14:13:52 -0600

    Completed Learning 04.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment = new Assignment("Levi Nelson", "Computer Science");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Cory Tillet", "Math 150x", "Section 7.3", "Problems 10-15");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WrittenAssignment writtenAssignment = new WrittenAssignment("True Herbert", "American History", "The Revolutionary War");
+        Console.WriteLine(writtenAssignment.GetSummary());
+        Console.WriteLine(writtenAssignment.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WrittenAssignment.cs
@@ -0,0 +1,15 @@
+public class WrittenAssignment : Assignment
+{
+    private string _title;
+
+    public WrittenAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {GetStudentName()}";
+    }
+}
